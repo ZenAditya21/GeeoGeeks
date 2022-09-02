ccd34036c38a2dba3e4fdf415dca1f2dcbd18229
ZenAditya21 <93545664+ZenAditya21@users.noreply.github.com>
2022-09-02 21:24:01 +0530

    Initial commit

---
+++ Quiz Game/quiz.js
@@ -0,0 +1,41 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "Guess the flag",
+  answer: "India",
+  options: [
+    "Germany","Turkey","France","India"
+  ],
+  image: "https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/1200px-Flag_of_India.svg.png"
+},
+{
+  numb: 2,
+  question: "Guess the flag",
+  answer: "United State Of America",
+  options: [
+    "Russia","Vietnam","Australia","United State Of America"
+  ],
+  image: "https://cdn.britannica.com/33/4833-004-828A9A84/Flag-United-States-of-America.jpg"
+},
+{
+  numb: 3,
+  question: "Guess the flag",
+  answer: "China",
+  options: [
+    "China","Taiwan","Japan","Thailand"   
+  ],
+  image:"https://media.istockphoto.com/photos/chinese-flag-picture-id452606475?k=20&m=452606475&s=612x612&w=0&h=0qoA6nDRI5P83PU8Qdwd-fy2-EBJLPcsmlwVb1E4Fpo="
+},
+{
+  numb: 3,
+  question: "Guess the flag",
+  answer: "China",
+  options: [
+    "China","Taiwan","Japan","Thailand"   
+  ],
+  image:"https://media.istockphoto.com/photos/chinese-flag-picture-id452606475?k=20&m=452606475&s=612x612&w=0&h=0qoA6nDRI5P83PU8Qdwd-fy2-EBJLPcsmlwVb1E4Fpo="
+},
+
+
+];
